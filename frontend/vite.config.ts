9243f3ecd8f82e71b3a3b8ecd4b6f625cc2e1699
Zaad1704 <zaad4041@gmail.com>
2025-06-09 11:57:40 +0600

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Point to the correct entry file inside the public folder
+      input: 'public/index.html',
+    },
+  },
 })
