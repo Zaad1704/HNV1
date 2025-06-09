0b55103d40ad8ac2511b6cf6cb8a4b6c86e55d6b
Zaad1704 <zaad4041@gmail.com>
2025-06-09 13:15:23 +0600

    Update billingController.ts

--- backend/controllers/billingController.ts
+++ backend/controllers/billingController.ts
@@ -1,7 +1,13 @@
+// FIX: Removed the duplicate import line.
 import { Request, Response, NextFunction } from "express";
 import Subscription from "../models/Subscription";
-import { billingPlans, createSubscriptionMock } from "../services/billingService";
-import { Request, Response, NextFunction } from 'express';
+// FIX: Corrected the function name based on previous error logs.
+import { billingPlans, createSubscription2CO } from "../services/billingService";
+
+// BEST PRACTICE: Define the shape of the request body.
+interface SubscribeBody {
+  planId: string;
+}
 
 // List available plans
 export async function getPlans(_req: Request, res: Response) {
@@ -11,7 +17,8 @@ export async function getPlans(_req: Request, res: Response) {
 // List billing history for current org
 export async function getBillingHistory(req: Request, res: Response, next: NextFunction) {
   try {
-    const orgId = req.user.orgId;
+    // This relies on your custom type definition for req.user to work.
+    const orgId = req.user.orgId; 
     const subs = await Subscription.find({ orgId });
     res.json(subs);
   } catch (err) {
@@ -20,23 +27,11 @@ export async function getBillingHistory(req: Request, res: Response, next: NextF
 }
 
 // Start new subscription (mock)
-export async function subscribe(req: Request, res: Response, next: NextFunction) {
+export async function subscribe(req: Request<{}, {}, SubscribeBody>, res: Response, next: NextFunction) {
   try {
     const { planId } = req.body;
+    // This relies on your custom type definition for req.user to work.
     const orgId = req.user.orgId;
     const userEmail = req.user.email;
 
-    const subData = await createSubscriptionMock(orgId, planId, userEmail);
-    const sub = await Subscription.create({
-      orgId,
-      plan: subData.plan,
-      status: subData.status,
-      renewalDate: subData.renewalDate,
-      externalId: subData.externalId,
-    });
-
-    res.json({ success: true, subscription: sub });
-  } catch (err) {
-    next(err);
-  }
-}
+    // FIX: Changed to use the correct imported
