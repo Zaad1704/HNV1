cfb0bfb2bfe38dacef8310ee833b7bcb12a48f95
Zaad1704 <zaad4041@gmail.com>
2025-06-09 11:56:13 +0600

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// The "import { resolve } from 'path'" is no longer needed.
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Since the build runs in the 'frontend' folder, 
-      // Vite just needs the filename.
-      input: 'index.html',
-    },
-  },
 })
