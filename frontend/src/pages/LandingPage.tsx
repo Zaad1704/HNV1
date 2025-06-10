11430cb942c443688dafa0ee38a10b0642b3aaa1
ragav420 <rajputragav420@gmail.com>
2025-06-10 12:40:58 +0600

    Update LandingPage.tsx

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import HeroSection from "../components/landing/HeroSection";
-// Import AboutSection, ServicesSection, LeadershipSection, ContactSection as you finish them!
+import AboutSection from "../components/landing/AboutSection";
+import ServicesSection from "../components/landing/ServicesSection";
+import LeadershipSection from "../components/landing/LeadershipSection";
+import ContactSection from "../components/landing/ContactSection";
+
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
-      {/* <AboutSection /> */}
-      {/* <ServicesSection /> */}
-      {/* <LeadershipSection /> */}
-      {/* <ContactSection /> */}
+      <AboutSection />
+      <ServicesSection />
+      <LeadershipSection />
+      <ContactSection />
     </>
   );
 }
