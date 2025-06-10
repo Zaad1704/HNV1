9c065f4588881beb54b59db031422e3564ce5201
ragav420 <rajputragav420@gmail.com>
2025-06-10 16:05:39 +0600

    Update useUsers.ts

--- frontend/src/hooks/useUsers.ts
+++ frontend/src/hooks/useUsers.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { api } from "../api/client";
+import apiClient from "../api/client";
 
 export function useUsers() {
   const [users, setUsers] = useState<any[]>([]);
