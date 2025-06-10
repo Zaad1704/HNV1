fb1f681d7b9fd8fb0dd506e23d8763437fe32b3d
ragav420 <rajputragav420@gmail.com>
2025-06-10 12:19:05 +0600

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-const HomePage = () => (
-  <div className="min-h-screen flex flex-col items-center justify-center">
-    <h1 className="text-4xl font-bold mb-4">Welcome to HNV SaaS Platform!</h1>
-    <div className="space-x-4">
-      <Link to="/login" className="p-2 bg-blue-600 text-white rounded">Login</Link>
-      <Link to="/register" className="p-2 bg-green-600 text-white rounded">Register</Link>
-      <Link to="/org/dashboard" className="p-2 bg-gray-600 text-white rounded">Dashboard</Link>
-    </div>
-  </div>
-);
-
-export default HomePage;
+import HeroSection from "../components/landing/HeroSection";
+// Import AboutSection, ServicesSection, LeadershipSection, ContactSection as you finish them!
+export default function LandingPage() {
+  return (
+    <>
+      <HeroSection />
+      {/* <AboutSection /> */}
+      {/* <ServicesSection /> */}
+      {/* <LeadershipSection /> */}
+      {/* <ContactSection /> */}
+    </>
+  );
+}
