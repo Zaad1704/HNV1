c79c38a0849844a31e0f73d581ba076813ef1140
ragav420 <rajputragav420@gmail.com>
2025-06-10 12:32:42 +0600

    Create LandingPage.tsx

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../components/landing/HeroSection";
+// Import AboutSection, ServicesSection, LeadershipSection, ContactSection as you finish them!
+export default function LandingPage() {
+  return (
+    <>
+      <HeroSection />
+      {/* <AboutSection /> */}
+      {/* <ServicesSection /> */}
+      {/* <LeadershipSection /> */}
+      {/* <ContactSection /> */}
+    </>
+  );
+}
