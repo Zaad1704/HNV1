e1f6e343c04a8754c73dd9685dd1419fcd64fda7
ragav420 <rajputragav420@gmail.com>
2025-06-10 11:59:56 +0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,15 @@
-// frontend/src/App.tsx
-
 import React from 'react';
-// FIX: Import Routes and Route from react-router-dom
 import { Routes, Route } from 'react-router-dom';
 
-// Import your page components and ProtectedRoute
+import HomePage from "./pages/HomePage";
 import OrganizationDashboardPage from "./pages/OrganizationDashboardPage";
 import AcceptInvitePage from "./pages/AcceptInvitePage";
-import ProtectedRoute from './components/ProtectedRoute'; // Assuming this is the path
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    // FIX: All <Route> components are wrapped in a <Routes> component
     <Routes>
-      {/* Add your other routes here, like the homepage */}
-      <Route path="/" element={<div>Welcome Home!</div>} />
-
+      <Route path="/" element={<HomePage />} />
       <Route 
         path="/org/dashboard" 
         element={
@@ -25,8 +19,6 @@ function App() {
         } 
       />
       <Route path="/invite/:token" element={<AcceptInvitePage />} />
-      
-      {/* Add a catch-all route for pages that don't exist */}
       <Route path="*" element={<div>404 - Page Not Found</div>} />
     </Routes>
   );
