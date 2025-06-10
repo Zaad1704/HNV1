aca0ade3fb630ba1f98680f11fb885ddcb807e14
ragav420 <rajputragav420@gmail.com>
2025-06-10 11:40:45 +0600

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
+// frontend/src/main.tsx
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // Make sure BrowserRouter is here
 import App from "./App";
 import "./index.css";
 
@@ -10,6 +12,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      {/* FIX: BrowserRouter wraps your entire App here */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
