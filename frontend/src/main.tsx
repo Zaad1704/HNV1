8303e215ebbd97efe8e72f4d237faf12a6b704d8
ragav420 <rajputragav420@gmail.com>
2025-06-10 13:45:43 +0600

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { LangProvider } from "./contexts/LanguageContext";
-import App from "./App";
-import "./index.css";
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <LangProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </LangProvider>
-  </React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// For this test, we are removing Suspense and i18n to simplify.
+// import './services/i18n';
+
+// We are also removing React.StrictMode to eliminate it as a potential issue.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 );
