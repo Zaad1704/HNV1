335251867ec390c26819cde7e56fc9baf39897bc
ragav420 <rajputragav420@gmail.com>
2025-06-10 12:51:27 +0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import LandingPage from "./pages/LandingPage";
-// import AboutPage, ServicesPage, ContactPage, etc. as you build them!
+import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";
+import LeadershipPage from "./pages/LeadershipPage";
+import ContactPage from "./pages/ContactPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <>
@@ -11,9 +16,11 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          {/* <Route path="/about" element={<AboutPage />} /> */}
-          {/* <Route path="/services" element={<ServicesPage />} /> */}
-          {/* <Route path="/contact" element={<ContactPage />} /> */}
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/leadership" element={<LeadershipPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </main>
       <Footer />
