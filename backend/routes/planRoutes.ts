ee197a24fd3a4d5f2fa1a4ba26112108fe7d462b
ragav420 <rajputragav420@gmail.com>
2025-06-16 10:10:07 +0600

    Update planRoutes.ts

--- backend/routes/planRoutes.ts
+++ backend/routes/planRoutes.ts
@@ -4,13 +4,12 @@ import { protect, authorize } from '../middleware/authMiddleware';
 
 const router = Router();
 
-// This route is public so the landing page can display plans.
+// This makes the GET request for all plans public
 router.route('/')
     .get(getPlans)
-    // The POST route to create a plan remains protected.
     .post(protect, authorize('Super Admin'), createPlan);
 
-// These routes for updating and deleting remain protected.
+// These routes remain protected
 router.route('/:id')
     .put(protect, authorize('Super Admin'), updatePlan)
     .delete(protect, authorize('Super Admin'), deletePlan);
