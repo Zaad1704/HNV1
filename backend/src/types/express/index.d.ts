c9dd7c373045a4efd57288f806a6767ed2726162
Zaad1704 <zaad4041@gmail.com>
2025-06-19 20:47:04 +0600

    Update index.d.ts

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,11 +1,2 @@
-import { IUser } from '../../models/User';
-
-declare global {
-  namespace Express {
-    export interface Request {
-      user?: IUser;
-    }
-  }
-}
-
-export {};
+// This file is intentionally left empty to avoid conflicts with express user typing.
+// All typing for req.user is handled in backend/src/types/express/index.d.ts
