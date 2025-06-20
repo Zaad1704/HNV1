50618e452de666dd3d87dae25f55a0ccee34b4ac
Zaad1704 <zaad4041@gmail.com>
2025-06-21 01:03:28 +0600

    Update superAdminRoutes.ts

--- backend/routes/superAdminRoutes.ts
+++ backend/routes/superAdminRoutes.ts
@@ -7,8 +7,9 @@ import {
     grantLifetimeAccess,
     revokeLifetimeAccess,
     updateUserByAdmin,
-    getModerators,      // <-- NEW
-    getGlobalBilling    // <-- NEW
+    getModerators,
+    getGlobalBilling,
+    updateSubscriptionStatus
 } from '../controllers/superAdminController';
 import { protect } from '../middleware/authMiddleware';
 import { authorize } from '../middleware/rbac';
@@ -19,13 +20,13 @@ router.use(protect, authorize(['Super Admin', 'Super Moderator']));
 router.get('/dashboard-stats', getDashboardStats);
 
 router.get('/organizations', getAllOrganizations);
+router.put('/organizations/:id/status', updateSubscriptionStatus);
 router.put('/organizations/:id/grant-lifetime', grantLifetimeAccess);
 router.put('/organizations/:id/revoke-lifetime', revokeLifetimeAccess);
 
 router.get('/users', getAllUsers);
 router.put('/users/:userId/manage', updateUserByAdmin);
 
-// NEW ROUTES
 router.get('/moderators', getModerators);
 router.get('/billing', getGlobalBilling);
 
