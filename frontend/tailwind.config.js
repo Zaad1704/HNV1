c3d6a58c6108db59cee548951d93a08c58ca1ea1
Zaad1704 <zaad4041@gmail.com>
2025-06-20 22:48:15 +0600

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,14 @@ export default {
        fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
+       colors: {
+        'brand-orange': '#FF7A00',
+        'light-bg': '#F7F8FA',
+        'light-card': '#FFFFFF',
+        'dark-text': '#1F2937',
+        'light-text': '#6B7280',
+        'border-color': '#E5E7EB',
+      }
     },
   },
   plugins: [],
