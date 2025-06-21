1d09c7fa05422d49721ad59ce3ac83b0a4de3e2c
Zaad1704 <zaad4041@gmail.com>
2025-06-21 09:51:52 +0600

    Update useProperties.ts

--- frontend/src/hooks/useProperties.ts
+++ frontend/src/hooks/useProperties.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { api } from "../api/client";
+import apiClient from "../api/client";
 
 export function useProperties() {
   return useQuery(["properties"], async () => {
-    const { data } = await api.get("/properties");
+    const { data } = await apiClient.get("/properties");
     return data;
   });
 }
