26d9e5e808b74c9ee28b7157ffb128fcdbab804a
Zaad1704 <zaad4041@gmail.com>
2025-06-21 20:06:17 +0600

    Update PublicLayout.tsx

--- frontend/src/components/layout/PublicLayout.tsx
+++ frontend/src/components/layout/PublicLayout.tsx
@@ -4,16 +4,18 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar';
 import Footer from './Footer';
+import PublicBottomNavBar from './PublicBottomNavBar'; // Import the new bottom nav
 
 const PublicLayout = () => {
   return (
     <div className="bg-slate-900">
       <Navbar />
       <main>
-        {/* The Navbar component has the logo which links to "/" */}
         <Outlet />
       </main>
       <Footer />
+      {/* Render PublicBottomNavBar only on screens smaller than md */}
+      <PublicBottomNavBar />
     </div>
   );
 };
