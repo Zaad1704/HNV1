0c1991531fd0db48912d28cfbe7393fab9bb749c
Zaad1704 <zaad4041@gmail.com>
2025-06-21 09:52:44 +0600

    Delete backen directory

--- backen/index.ts
+++
@@ -1,9 +0,0 @@
-// Optional: If you want to aggregate all routes for easy import
-import { Express } from "express";
-import authRoutes from "./authRoutes";
-import userRoutes from "./userRoutes";
-
-export default (app: Express) => {
-  app.use("/api/auth", authRoutes);
-  app.use("/api/users", userRoutes);
-};
