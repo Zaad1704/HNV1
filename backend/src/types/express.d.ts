1960d992fedd936e3b02c2f5b9e78d0ca00a4e75
Zaad1704 <zaad4041@gmail.com>
2025-06-23 22:14:47 +0600

    Update express.d.ts

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,11 +1,10 @@
-// backend/src/types/express.d.ts
-
 import { Types, Document } from 'mongoose';
 import { IUser } from '../../models/User';
 
-// This uses module augmentation to add the 'user' property to the Express Request interface globally.
 declare module 'express-serve-static-core' {
   interface Request {
     user?: (IUser & Document<any, any, any>) | null;
+    // FIX: Add the organizationId property
+    organizationId?: Types.ObjectId;
   }
 }
