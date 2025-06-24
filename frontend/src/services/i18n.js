ec3dbe0a28130944616bc82b8c98a038f95c83a8
Zaad1704 <zaad4041@gmail.com>
2025-06-24 12:49:06 +0600

    Update i18n.js

--- frontend/src/services/i18n.js
+++ frontend/src/services/i18n.js
@@ -4,28 +4,26 @@ import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  .use(HttpApi) // Loads translations from your /public/locales folder
-  .use(LanguageDetector) // Detects user language
-  .use(initReactI18next) // Passes i18n down to react-i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    // Remove the large 'resources' object
-    supportedLngs: ['en', 'bn'], // Add all supported languages here
+    // Add the 'react' block below
+    react: {
+      useSuspense: true, // This is the key change
+    },
+    supportedLngs: ['en', 'bn'],
     fallbackLng: 'en',
-    debug: process.env.NODE_ENV === 'development', // Enable debug output in development
-
-    // Options for language detector
+    debug: process.env.NODE_ENV === 'development',
     detection: {
       order: ['queryString', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
       caches: ['cookie'],
     },
-
-    // Options for http backend
     backend: {
       loadPath: '/locales/{{lng}}/translation.json',
     },
-
     interpolation: {
-      escapeValue: false, // React already safes from xss
+      escapeValue: false, 
     },
   });
 
