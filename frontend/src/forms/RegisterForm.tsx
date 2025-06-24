b235cb45a979eb75a795a64ed029ef4002b705f7
Zaad1704 <zaad4041@gmail.com>
2025-06-24 21:40:50 +0600

    Update RegisterForm.tsx

--- frontend/src/forms/RegisterForm.tsx
+++ frontend/src/forms/RegisterForm.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { registerSchema } from "../../validators/userValidator";
+import { registerSchema } from "../../../backend/validators/userValidator";
 
 // ... useForm({ resolver: zodResolver(registerSchema) })
