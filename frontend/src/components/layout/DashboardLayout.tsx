6e3377ac8e6cd076c5b5749550358a5c0a6bd508
Zaad1704 <zaad4041@gmail.com>
2025-06-26 12:00:38 +0600

    Update DashboardLayout.tsx

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
-import useAuthStore from '../../store/authStore';
+import { useAuthStore } from '../../store/authStore';
 
 // ...UI icons...
 
